61b536f9ce4e1e8c1d9dcb332e68bea2beb15559
Fabian <business.fabian@gmx.de>
2026-04-18 21:12:30 +0200

    fix: Favicon auf 64px gerendert fuer schärfere Darstellung

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,6 +1,6 @@
 import { ImageResponse } from "next/og"
 
-export const size = { width: 32, height: 32 }
+export const size = { width: 64, height: 64 }
 export const contentType = "image/png"
 
 // Variante D: Klassisch & Dezent -- dunkles Quadrat, SQ weiss, oranger Unterstrich
@@ -21,27 +21,26 @@ export default function Icon() {
       >
         <span
           style={{
-            fontSize: 15,
-            fontWeight: 800,
+            fontSize: 30,
+            fontWeight: 900,
             color: "#ffffff",
-            letterSpacing: -0.5,
+            letterSpacing: -1,
             fontFamily: "sans-serif",
-            marginBottom: 3,
+            marginBottom: 6,
           }}
         >
           SQ
         </span>
-        {/* Unterstrich-Akzent, dicker fuer bessere Sichtbarkeit bei 32px */}
         <div
           style={{
             position: "absolute",
-            bottom: 4,
+            bottom: 8,
             left: "50%",
             transform: "translateX(-50%)",
-            width: 18,
-            height: 3,
+            width: 36,
+            height: 5,
             background: "#FF6B00",
-            borderRadius: 1,
+            borderRadius: 2,
           }}
         />
       </div>
